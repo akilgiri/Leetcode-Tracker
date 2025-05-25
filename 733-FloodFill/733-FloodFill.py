ef72a50a77cade47d1007f232d2247d27c9e34f5
Akil Giri <55789307+akilgiri@users.noreply.github.com>
2025-05-25 13:08:17 -0400

    Create 733-FloodFill

---
+++ 733-FloodFill/733-FloodFill.py
@@ -0,0 +1,18 @@
+# Last updated: 5/25/2025, 1:08:17 PM
+class Solution:
+    def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
+        cur_color = image[sr][sc]
+
+        height = len(image)
+        width = len(image[0])
+
+        def dfs(sr, sc):
+            if 0 <= sr < height and 0 <= sc < width and image[sr][sc] == cur_color and image[sr][sc] != color:
+                image[sr][sc] = color
+                dfs(sr+1, sc)
+                dfs(sr-1, sc)
+                dfs(sr, sc+1)
+                dfs(sr, sc-1)
+
+        dfs(sr, sc)
+        return image
