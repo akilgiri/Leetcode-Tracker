305ff1ba64bacd50ca3a98f63b5a6c96c4500c1a
Akil Giri <55789307+akilgiri@users.noreply.github.com>
2025-05-25 13:08:11 -0400

    Create 2732-Counter

---
+++ 2732-Counter/2732-Counter.js
@@ -0,0 +1,18 @@
+// Last updated: 5/25/2025, 1:08:11 PM
+/**
+ * @param {number} n
+ * @return {Function} counter
+ */
+var createCounter = function(n) {
+    
+    return function() {
+        return n++;
+    };
+};
+
+/** 
+ * const counter = createCounter(10)
+ * counter() // 10
+ * counter() // 11
+ * counter() // 12
+ */
