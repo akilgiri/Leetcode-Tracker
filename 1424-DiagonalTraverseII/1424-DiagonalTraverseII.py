717d14ecfd84e9707812a283732126588361eb19
Akil Giri <55789307+akilgiri@users.noreply.github.com>
2025-06-02 07:09:07 -0400

    File updated at 6/2/2025, 7:09:07 AM

--- 1424-DiagonalTraverseII/1424-DiagonalTraverseII.py
+++ 1424-DiagonalTraverseII/1424-DiagonalTraverseII.py
@@ -1,15 +1,22 @@
-# Last updated: 6/2/2025, 7:08:12 AM
+# Last updated: 6/2/2025, 7:09:07 AM
 class Solution:
-    def findDiagonalOrder(self, nums: List[List[int]]) -> List[int]:
-        groups = defaultdict(list)
-        result = []
+    # def findDiagonalOrder(self, nums: List[List[int]]) -> List[int]:
+    #     groups = defaultdict(list)
+    #     result = []
 
-        for i in range(len(nums)):
-            for j in range(len(nums[i])):
-                print(i,j)
-                groups[i+j].append(nums[i][j])
+    #     for i in range(len(nums)):
+    #         for j in range(len(nums[i])):
+    #             print(i,j)
+    #             groups[i+j].append(nums[i][j])
 
-        for g in groups.values():
-            result += g[::-1]
+    #     for g in groups.values():
+    #         result += g[::-1]
 
-        return result
+    #     return result
+
+    def findDiagonalOrder(self, A: List[List[int]]) -> List[int]:
+        d = defaultdict(list)
+        for i in range(len(A)):
+            for j in range(len(A[i])):
+                d[i+j].append(A[i][j])
+        return [v for k in d.keys() for v in reversed(d[k])]
